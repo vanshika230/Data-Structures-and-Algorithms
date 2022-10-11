312ca6d60264cb19829fafb6491795ee4d71173d
Ayush Sharma <62910168+ayushtenguriya@users.noreply.github.com>
2022-10-11 18:26:25 +0530

    DSA

---
+++ linkedin imp.java
@@ -0,0 +1,14 @@
+public class LinkedList {
+	Node head; // head of list
+
+	/* Linked list Node*/
+	class Node {
+		int data;
+		Node next;
+
+		// Constructor to create a new node
+		// Next is by default initialized
+		// as null
+		Node(int d) { data = d; }
+	}
+}
