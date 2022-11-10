0c991c09d0302c2e6a1d7f1846a8cea1446ee1e4
Vanshika Mishra <74042272+vanshika230@users.noreply.github.com>
2022-11-10 12:45:17 +0530

    Update Search_in_sorted_array.java

--- 04twoD_Arrays/Search_in_sorted_array.java
+++ 04twoD_Arrays/Search_in_sorted_array.java
@@ -1,32 +1,41 @@
 import java.io.*;
 import java.util.*;
 
-public class Search_in_sorted_array {
+public class Search_in_sorted_2d_array {
 
     public static void main(String[] args) throws Exception {
         // write your code here
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int n = Integer.parseInt(br.readLine());
-      int[][] arr = new int[n][n];
+   import java.util.*;
 
 
-      for (int i = 0; i < n; i++) {
-         for (int j = 0; j < n; j++) {
-            arr[i][j] = Integer.parseInt(br.readLine());
-         }
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[][] arr = new int[n][n];
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        arr[i][j] = scn.nextInt();
       }
-      int x= Integer.parseInt(br.readLine());
-      
-      for (int i = 0; i < n; i++) {
-         for (int j = 0; j < n; j++) {
-            if(arr[i][j] == x){
-                System.out.println(i);
-                System.out.println(j);
-                return;
-            }
-         }
-      }
-      System.out.println("Not Found");
     }
+    int data = scn.nextInt();
 
+    int row = 0;
+    int col = n - 1;
+    while (row < n && col >= 0) {
+      if (arr[row][col] == data) {
+        System.out.println(row);
+        System.out.println(col);
+        return;
+      }
+      if (arr[row][col] > data) {
+        col--;
+      } else {
+        row++;
+      }
+    }
+    System.out.print("Not Found");
+  }
+}
 }
