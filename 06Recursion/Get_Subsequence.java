c78848084cc709a19f52da33a2b69e4a10027280
Vanshika Mishra <74042272+vanshika230@users.noreply.github.com>
2022-12-19 23:09:55 +0530

    Update Get_Subsequence.java

--- 06Recursion/Get_Subsequence.java
+++ 06Recursion/Get_Subsequence.java
@@ -36,3 +36,41 @@ O(n) This time complexity is linear because a recursion call is made along with 
 SPACE COMPLEXITY :
 
 O(1) As no extra space is required, therefore space complexity is constant. However, if we include the space used in the recursive stack then the space complexity is O(n).*/
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+       
+         if (s.isEmpty()){
+            return true;
+        }
+        if(t.isEmpty()){
+            return false;
+        }
+        if(t.indexOf(s.charAt(0)) != -1 && s.length() <= 1){
+
+            return true;
+        } else if (t.indexOf(s.charAt(0)) != -1){
+            int firstChar = t.indexOf(s.charAt(0));
+            return isSubsequence(s.substring(1), t.substring(firstChar+1));
+        } else {
+            return false;
+        }
+    }
+
+
+}
+/*
+Intuition
+Check if the first char of s is in t.
+If it is in t find its index in t (int firstChar).
+Take Substring of s from the next char in s and substring of t starting from the char following the found index.
+Recurse using the substrings.
+return true if last char in s is found.
+return false if a char in s not found in t.
+Complexity
+Time complexity:
+O(n)
+
+Space complexity:
+O(1)
+*/
