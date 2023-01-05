8d81b01050f3b92aaa086d6aef51b23fe990a1ef
Vanshika Mishra <74042272+vanshika230@users.noreply.github.com>
2023-01-05 14:12:02 +0530

    Create ReverseLLPtrIterative.java

---
+++ 08LinkedList/ReverseLLPtrIterative.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode current=head;
+        ListNode previous=null;
+        ListNode next=null;
+
+    if(head==null)
+{
+return head;
+}
+while(current!=null)
+{
+next=current.next;
+current.next=previous;
+previous=current;
+current=next;
+}
+return previous;
+    }
+}
