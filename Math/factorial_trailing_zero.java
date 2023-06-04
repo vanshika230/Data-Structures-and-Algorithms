71c503a79476f1d312999905c4fba2902add6092
Vanshika Mishra <74042272+vanshika230@users.noreply.github.com>
2023-06-04 17:53:44 +0530

    Create factorial_trailing_zero.java

---
+++ Math/factorial_trailing_zero.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int getFact(int n) {
+        int count = 0;
+        while (n >= 5) {
+            n /= 5;
+            count += n;
+        }
+        return count;
+    }
+}
