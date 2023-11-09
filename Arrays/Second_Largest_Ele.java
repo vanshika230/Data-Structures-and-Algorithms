2cb079e5c0139c112be0c4e8aadef57c70a50e8e
Vanshika Mishra <74042272+vanshika230@users.noreply.github.com>
2023-11-09 21:32:43 +0530

    Create Second_Largest_Ele.java

---
+++ Arrays/Second_Largest_Ele.java
@@ -0,0 +1,64 @@
+
+
+
+import java.io.*;
+class Test
+{
+static private int secondSmallest(int[] arr, int n)
+{
+	if (n < 2)
+	{
+		return -1;
+	}
+	int small = Integer.MAX_VALUE;
+	int second_small = Integer.MAX_VALUE;
+	int i;
+	for (i = 0; i < n; i++)
+	{
+	   if (arr[i] < small)
+	   {
+		  second_small = small;
+		  small = arr[i];
+	   }
+	   else if (arr[i] < second_small && arr[i] != small)
+	   {
+		  second_small = arr[i];
+	   }
+	}
+   return second_small;
+}
+static private int secondLargest(int[] arr, int n)
+{
+	if(n<2)
+	return -1;
+	int large = Integer.MIN_VALUE;
+	int second_large = Integer.MIN_VALUE;
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		if (arr[i] > large)
+		{
+			second_large = large;
+			large = arr[i];
+		}
+
+		else if (arr[i] > second_large && arr[i] != large)
+		{
+			second_large = arr[i];
+		}
+	}
+	return second_large;
+}
+
+public static void main(String[] args)
+{
+	int[] arr = {1, 2, 4, 7, 7, 5};
+	int n = arr.length;
+		int sS = secondSmallest(arr, n);
+		int sL = secondLargest(arr, n);
+	System.out.println("Second smallest is "+sS);
+	System.out.println("Second largest is "+sL);
+}
+
+}
+
