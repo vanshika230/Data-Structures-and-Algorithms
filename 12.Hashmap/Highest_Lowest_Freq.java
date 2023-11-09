729a46c759bc087a14acc40c62f7ff7ae6785699
Vanshika Mishra <74042272+vanshika230@users.noreply.github.com>
2023-11-09 20:29:53 +0530

    Create Highest_Lowest_Freq.java

---
+++ 12.Hashmap/Highest_Lowest_Freq.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        int arr[] = {10, 5, 10, 15, 10, 5};
+        int n = arr.length;
+        Frequency(arr, n);
+    }
+    static void Frequency(int arr[], int n) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+
+        int maxFreq = 0, minFreq = n;
+        int maxEle = 0, minEle = 0;
+        // Traverse through map and find the elements
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int count = entry.getValue();
+            int element = entry.getKey();
+
+            if (count > maxFreq) {
+                maxEle = element;
+                maxFreq = count;
+            }
+            if (count < minFreq) {
+                minEle = element;
+                minFreq = count;
+            }
+        }
+
+        System.out.println("The highest frequency element is: " + maxEle);
+        System.out.println("The lowest frequency element is: " + minEle);
+    }
+} 
